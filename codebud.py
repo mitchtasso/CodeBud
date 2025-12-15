d55b420b7f16bb9c3bc2b03056dbfb3020a1d0b6
mitchtasso <145378736+mitchtasso@users.noreply.github.com>
2025-12-15 15:38:18 -0500

    Refactor CodeBud to use updated chat import and removed basic chat due to existing ollama basic chat

--- codebud.py
+++ codebud.py
@@ -1,4 +1,4 @@
-import ollama
+from ollama import chat
 import sys
 
 model = ''
@@ -7,122 +7,100 @@ arguments = sys.argv
 messages = []
 
 def main_loop():
-  if len(arguments) > 1:
-    model = arguments[1]
+  if len(arguments) > 2:
+      messages = []
+      try:
 
-    if len(arguments) > 2:
-        messages = []
-        try:
+        with open(arguments[2], 'r') as f:
+          textFile = f.read()
+        
+        print('----READ FILE----\n\n')
+        print(textFile)
+        print('\n\n----END READ----\n\n')
 
-          with open(arguments[2], 'r') as f:
-            textFile = f.read()
-          
-          print('----READ FILE----\n\n')
-          print(textFile)
-          print('\n\n----END READ----\n\n')
-
+        
+        if len(arguments) > 3:
           messages.append({
-            'role': 'system',
-            'content': 'You are a programmer named CodeBud. Read the contents of this file and provide a very short analysis: ' + textFile
-          })
+          'role': 'system',
+          'content': 'You are a programmer named CodeBud. Read the contents of this file for context: ' + textFile + "\n Perform or answer this prompt, keep responses short always: " + arguments[3]
+        })
+        else:
+          messages.append({
+          'role': 'system',
+          'content': 'You are a programmer named CodeBud. Read the contents of this file and wait for further instructions: ' + textFile
+        })
 
-          response = ollama.chat(model=model, messages=messages, stream=True)
+        try:
+          response = chat(model=model, messages=messages, stream=True)
+          print("Model: " + arguments[1] + "\n")
+
           analysisResponse = ''
           for chunk in response:
               print(chunk['message']['content'], end='', flush=True)
               analysisResponse += chunk['message']['content']
+        except:
+          print('error: model not installed or was typed incorrectly')
+          sys.exit(0)
 
+        messages.append(
+        {
+          'role': 'assistant',
+          'content': analysisResponse
+        }
+        )
+        
+        while (True):
+          choice_input = input("\n\nPrompt(q to quit): ")
+          print('\n')
           messages.append(
-          {
-            'role': 'assistant',
-            'content': analysisResponse
-          }
+            {
+              'role': 'user',
+              'content': choice_input
+            }
           )
+
+          if choice_input == 'q':
+            break
           
-          while (True):
-            choice_input = input("\n\nPrompt(q to quit): ")
-            print('\n')
-            messages.append(
-              {
-                'role': 'user',
-                'content': choice_input
-              }
-            )
+          response = chat(model=model, messages=messages, stream=True)
+          modelResponse = ''
+          if choice_input != 'q':
+            for chunk in response:
+                print(chunk['message']['content'], end='', flush=True)
+                modelResponse += chunk['message']['content']
+          
+          messages.append(
+            {
+              'role': 'assistant',
+              'content': modelResponse
+            }
+          )
+          print('\n')
 
-            if choice_input == 'q':
-              break
-            
-            response = ollama.chat(model=model, messages=messages, stream=True)
-            modelResponse = ''
-            if choice_input != 'q':
-              for chunk in response:
-                  print(chunk['message']['content'], end='', flush=True)
-                  modelResponse += chunk['message']['content']
-            
-            messages.append(
-              {
-                'role': 'assistant',
-                'content': modelResponse
-              }
-            )
-            print('\n')
+      except FileNotFoundError:
+        print('error: file not found')
 
-        except FileNotFoundError:
-          print('error: file not found')
-
-    else:
-      messages = [
-          {
-            'role': 'system',
-            'content': 'You are a programmer named CodeBud. Do not provide explanations for code unless asked.'
-          }]
-      
-      while (True):
-        choice_input = input("\nPrompt(q to quit): ")
-        print('\n')
-        messages.append(
-          {
-            'role': 'user',
-            'content': choice_input
-          }
-        )
-
-        if choice_input == 'q':
-          break
-        
-        response = ollama.chat(model=model, messages=messages, stream=True)
-        modelResponse = ''
-        if choice_input != 'q':
-          for chunk in response:
-              print(chunk['message']['content'], end='', flush=True)
-              modelResponse += chunk['message']['content']
-        
-        messages.append(
-          {
-            'role': 'assistant',
-            'content': modelResponse
-          }
-        )
-        print('\n')
+  else:
+      print('error: please include the filename after the model name for the analysis to start')
+      print('To start a basic chat with your ollama model, run: ollama run [model name]')
 
 if __name__ == "__main__":
-  print("Welcome to CodeBud!")
+  print("CodeBud v1.1")
   try:
     if arguments[1] == '--help':
-      print('Version: 1.0\n')
       print('----HELP----')
-      print('How to format a basic chat command: codebud gemma3:1b #codebud [model name]')
-      print('How to format a file read chat command: codebud gemma3:1b example.txt #codebud [model name] [path to file]')
+      print('How to format a basic chat command: ollama run gemma3:1b #ollama run [model name]')
+      print('How to format a basic file read chat command: codebud gemma3:1b example.txt #codebud [model name] [path to file]')
+      print('How to format a basic file read chat command with initial prompt: codebud gemma3:1b example.txt "Tell me what this file does." #codebud [model name] [path to file] ["prompt"]')
       print('\nMUST HAVE OLLAMA AND MODELS INSTALLED FOR CHAT TO WORK')
       print('Ollama install cmd: curl -fsSL https://ollama.com/install.sh | sh')
       print('Ollama model install cmd: ollama run [model name]')
-      exit()
-    else:
-      test = ollama.chat(model=arguments[1], messages=[])
-      print("Model: " + arguments[1] + "\n")
-  except:
+      print('Ollama cmd to check your current installed models: ollama list')
+      sys.exit(0)
+  except IndexError:
     print("Error: No model specified to run the program. Or model specified is not typed correctly.\n")
     print("Please run 'codebud --help' for more information.")
-    exit()
+    sys.exit(0)
 
+  model = arguments[1]
   main_loop()
